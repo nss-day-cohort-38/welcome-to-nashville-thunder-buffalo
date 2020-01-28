56966584a50fbdfdf21db2d8c28d3cbc2af5203a
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-28 09:31:50 -0600

    changed search results to cuisines

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -2,9 +2,10 @@ const restaurantApiKey = apiKeys.restaurantKey;
 
 const restaurantApi = {
 getRestaurants () {
-return fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&apikey=${restaurantApiKey}`)
+return fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138
+&apikey=${restaurantApiKey}`)
     .then(r => r.json())
     .then(parsedRestaurants => {
         console.log(parsedRestaurants)
     })}}
-    restaurantApi.getRestaurants()
+   
