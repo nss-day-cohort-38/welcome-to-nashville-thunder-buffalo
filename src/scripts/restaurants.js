36b053589ae638551349d63a9e9f837fe8512f3c
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-28 10:51:04 -0600

    saving jic

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,9 +1,9 @@
 const restaurantApiKey = apiKeys.restaurantKey;
 
+const restaurantURL = `https://developers.zomato.com/api/v2.1/cuisines?city_id=1138&apikey=${restaurantApiKey}`
 const restaurantApi = {
-getRestaurants () {
-return fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138
-&apikey=${restaurantApiKey}`)
+getRestaurants () { console.log(restaurantURL)
+return fetch(restaurantURL)
     .then(r => r.json())
     .then(parsedRestaurants => {
         console.log(parsedRestaurants)
