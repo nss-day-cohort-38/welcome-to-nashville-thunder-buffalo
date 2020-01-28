e831cf466337591846f8366425b54de72936cd17
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-28 14:40:12 -0600

    search results printing to the dom

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,13 +1,32 @@
 const restaurantApiKey = apiKeys.restaurantKey;
+let containerResults = document.querySelector("#resultsContainer")
 
-const restaurantURL = `https://developers.zomato.com/api/v2.1/cuisines?city_id=1138&apikey=${restaurantApiKey}`
+const restaurantURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=greek&sort=rating&apikey=${restaurantApiKey}`
 const restaurantApi = {
-getRestaurants () { console.log(restaurantURL)
+getRestaurants () { console.log()
 return fetch(restaurantURL)
     .then(r => r.json())
     .then(parsedRestaurants => {
         console.log(parsedRestaurants)
+        const restaurantResults =
+            parsedRestaurants.restaurants
+console.log(restaurantResults)
+      restaurantResults.forEach(restaurants => {
+        const restaurantName = restaurants.restaurant.name
+    console.log(restaurantName)  
+    const restAsHTML = restResults(restaurantName)
+    restaurantToDom (restAsHTML)
+      });  
     })}}
-   const restaurantSearch = {
-
-   }
+  
+ const restResults = (name, index) => {
+     return `
+     <ul>
+     <li>${name}</li>
+     <li><button id=restButton-${index}>Save</button></li>
+     </ul>`
+ }
+ const restaurantToDom = (name) => {
+     containerResults.innerHTML += name
+     
+ }
