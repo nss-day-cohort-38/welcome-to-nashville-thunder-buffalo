cc775fcfe00a4be80d0fe4e3e3dda24eeec1da9a
castle <castlesmadeofcode@gmail.com>
2020-01-28 11:21:02 -0600

    concert search button is now functional

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -1,23 +1,59 @@
 
 const concertAPIKey= apiKeys.concertKey;
 const concertUserInput = "";
-// keyword=${concertUserInput}
+let concertKeyword = "";
+
+
+
 const concertAPI = {
-    getConcerts() {
-        // keyword= will need to match text typed into concerts search feature 
-      return fetch(`http://app.ticketmaster.com/discovery/v2/events.json?keyword=Rock&city=Nashville&apikey=${concertAPIKey}`)
+    getConcerts(concertKeyword) {
+      return fetch(`http://app.ticketmaster.com/discovery/v2/events.json?keyword=${concertKeyword}&city=Nashville&apikey=${concertAPIKey}`)
         .then(response => response.json())
         .then(parsedConcerts => {
-            console.log(parsedConcerts);
             const parsedEvents = parsedConcerts._embedded.events
-            parsedEvents.forEach(concert => {
-                console.log(concert);
-
+            parsedEvents.forEach(concert => {     
+                const genre = concert.classifications[0].genre.name;
+                   if (genre === concertKeyword) {
+                       const artist = concert.name
+                       const venue = concert._embedded.venues[0].name;
+                       const concertAsHTML = makeConcertEntry(artist, venue);
+                       insertConcertToDom(concertAsHTML);
+                   }
                 })
-
-    })
+                })
     }   
   };
 
-  concertAPI.getConcerts();
+
+
+  const makeConcertEntry = (artist, venue) => {
+    return `
+        <ul>
+        <li>Artist: ${artist}</li>
+        <li>Venue: ${venue}</li>
+        <li><button id="concertButton">Save</button><li>
+        </ul>
+    `
+}
+
+const insertConcertToDom= (concert) => {
+    const resultsContainer = document.querySelector("#resultsContainer");
+    resultsContainer.innerHTML += concert;
+
+
+}
+
+const captureUserInfo = (event) => {
+    const concertSearchText = document.getElementById("concertText");
+     concertKeyword = concertSearchText.value;
+    console.log(concertKeyword);
+    concertAPI.getConcerts(concertKeyword);
+}
+document.getElementById("concertsButton").addEventListener("click", captureUserInfo);
+
+
+    
+
+
+
 
