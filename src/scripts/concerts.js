c2812eba7fba317831d1184a59f9ac575f5a2273
castle <castlesmadeofcode@gmail.com>
2020-01-29 11:20:44 -0600

    hit mvp for welcome to nashville group projects (concerts)

--- src/scripts/concerts.js
+++ src/scripts/concerts.js
@@ -2,7 +2,8 @@
 const concertAPIKey= apiKeys.concertKey;
 const concertUserInput = "";
 const concertSearchText = document.getElementById("concertText");
-let resultsContainer = document.querySelector("#resultsContainer");
+const resultsContainer = document.querySelector("#resultsContainer");
+const concertsContainer = document.getElementById("concertName");
 
 
 
@@ -18,8 +19,7 @@ const concertAPI = {
                    if (genre === concertKeyword) {
                        const artist = concert.name
                        const venue = concert._embedded.venues[0].name;
-                       console.log(i);
-                       const concertAsHTML = makeConcertEntry(artist, venue, i);            
+                       const concertAsHTML = makeConcertEntry(artist, venue, i);
                        insertConcertToDom(concertAsHTML);
                    }
                 })
@@ -30,33 +30,103 @@ const concertAPI = {
 
 
   const makeConcertEntry = (artist, venue, index) => {
+
     return `
         <ul>
-        <li id=${index}>Artist: ${artist}</li>
-        <li id=${index}>Venue: ${venue}</li>
-        <li><button id="concertButton">Save</button><li>
+        <li id=${index}>
+        <span id=artistId-${index}>${artist} at</span>
+        <span id=venueId-${index}> ${venue} <span></li>
+        <li><button id=concertButton-${index}>Save</button><li>
         </ul>
     `
+
 }
 
 const insertConcertToDom= (concert) => {
     resultsContainer.innerHTML += concert;
-
-
 }
 
 const captureUserInfo = (event) => {
 
     concertKeyword = concertSearchText.value;
     concertAPI.getConcerts(concertKeyword);
-
     concertSearchText.value = "";
 }
+
 document.getElementById("concertsButton").addEventListener("click", captureUserInfo);
 
 
+const getButtonId = (event) => {
+    let buttonId = (event.target.id.split('-')[1]);
+    const concertDiv = document.getElementById(`concertButton-${buttonId}`);
+    favoriteEventManager.removeFavorite()
+    concertDiv.classList.add('favorite');
+    const artist = document.getElementById(`artistId-${buttonId}`);
+    artist.classList.add('favoriteArtist');
+    const venue = document.getElementById(`venueId-${buttonId}`);
+    venue.classList.add('favoriteVenue')
+
+    favoriteEventManager.addToItinerary();
     
+}
+
+document.getElementById("resultsContainer").addEventListener("click",getButtonId);
 
 
+
+const favoriteEventManager = {
+//     addFavorite() {
+//       const buttons = document.querySelectorAll("#concertButton-");
+//       console.log(buttons)
+//       for (let button of buttons) {
+//         button.addEventListener("click", getButtonId);
+//       }
+//     },
+     removeFavorite () {
+      const favorites = document.querySelectorAll(".favorite");
+      const artists = document.querySelectorAll(".favoriteArtist");
+      const venues = document.querySelectorAll(".favoriteVenue");
+
+
+      for (let favorite of favorites) {
+        favorite.classList.remove('favorite')
+      }
+      for (let artist of artists) {
+        artist.classList.remove('favoriteArtist')
+      }
+      for (let venue of venues) {
+        venue.classList.remove('favoriteVenue')
+      }
+    },
+    addToItinerary () {
+        const artist = document.querySelector('.favoriteArtist')
+        const venue = document.querySelector('.favoriteVenue')
+        const itinerary = concertsContainer;
+
+        itinerary.textContent = `Concerts: ${artist.textContent} ${venue.textContent}`
+}
+}
+
+
+
+//   favoriteEventManager.addFavorite();
+
+
+
+
+
+//   const runEventManager = () => {
+//     favoriteEventManager.removeFavorite();
+//     favoriteEventManager.addFavorite();
+//   }
+
+//   document.getElementById("resultsContainer").addEventListener("click",runEventManager);
+
+//   favoriteEventManager.removeFavoriteEventListeners();
+//   favoriteEventManager.addFavoriteEventListeners();
+
+
+//function that adds favoriteId to li
+//function that removes favoriteId and adds 
 
 
