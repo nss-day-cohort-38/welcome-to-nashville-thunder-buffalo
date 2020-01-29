447a46884dbe26b541d2ebc16e4a57753df5087b
Mollie Goforth <molliegoforth818@gmail.com>
2020-01-29 11:57:16 -0600

    save button consoling to the log

--- src/scripts/restaurants.js
+++ src/scripts/restaurants.js
@@ -1,41 +1,51 @@
 const restaurantApiKey = apiKeys.restaurantKey;
-let containerResults = document.querySelector("#resultsContainer")
+let resultsContainer = document.querySelector("#resultsContainer")
 const restaurantSearchText = document.getElementById("restaurantText")
 
 
 const restaurantApi = {
-getRestaurants (restKeyword) { console.log()
-    const restaurantURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${restKeyword}&sort=rating&apikey=${restaurantApiKey}`
-return fetch(restaurantURL)
-    .then(r => r.json())
-    .then(parsedRestaurants => {
-        console.log(parsedRestaurants)
-        const restaurantResults =
-            parsedRestaurants.restaurants
-console.log(restaurantResults)
-      restaurantResults.forEach(restaurants => {
-        const restaurantName = restaurants.restaurant.name
-    console.log(restaurantName)  
-    const restAsHTML = restResults(restaurantName)
-    restaurantToDom (restAsHTML)
-      });  
-    })}}
-  
- const restResults = (name, index) => {
-     return `
+    getRestaurants(restKeyword) {
+        console.log()
+        const restaurantURL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=${restKeyword}&sort=rating&apikey=${restaurantApiKey}`
+        return fetch(restaurantURL)
+            .then(r => r.json())
+            .then(parsedRestaurants => {
+
+                const restaurantResults =
+                    parsedRestaurants.restaurants
+
+                restaurantResults.forEach((restaurants, i) => {
+                    const restaurantName = restaurants.restaurant.name
+
+                    const restAsHTML = restResults(restaurantName, i)
+                    restaurantToDom(restAsHTML)
+                })
+            })
+    }
+}
+    ;
+
+
+const restResults = (name, index) => {
+    return `
      <ul>
-     <li>${name}</li>
+     <li id=restName-${index}>${name}</li>
      <li><button id=restButton-${index}>Save</button></li>
      </ul>`
- }
- const restaurantToDom = (name) => {
-    containerResults.innerHTML += name
-     
- }
- const searchRestButton = (event) => {
-     restKeyword = restaurantSearchText.value
-     restaurantApi.getRestaurants(restKeyword);
-     restaurantSearchText.value = "";
- }
- document.getElementById("restaurantsButton").addEventListener("click", searchRestButton); 
+}
+const restaurantToDom = (name) => {
+    resultsContainer.innerHTML += name
 
+}
+const searchRestButton = (event) => {
+    restKeyword = restaurantSearchText.value
+    restaurantApi.getRestaurants(restKeyword);
+    restaurantSearchText.value = "";
+}
+document.getElementById("restaurantsButton").addEventListener("click", searchRestButton);
+
+const getButtonsId = (event) => {
+    const getButtonsId = (event.target.id.split('-')[1]);
+    console.log(getButtonsId)
+}
+document.getElementById("resultsContainer").addEventListener("click", getButtonsId);
